941ad72f3f2c1fdc06f3e298f3eb6489344215c9
joaovictorgm <94790386+joaovictorgm@users.noreply.github.com>
2023-05-01 11:22:41 -0300

    Update index1.js

--- blog/index1.js
+++ blog/index1.js
@@ -2,7 +2,7 @@ const Author = require("./Author");
 
 const john = new Author("John Doe")
 
-const post = john.writePost("Titulo do psot", "Lorem ipsum dolor sic...")
+const post = john.writePost("Titulo do post", "Lorem ipsum dolor sic...")
 
 post.addComment("João", "Legal!")
 post.addComment("Victor", "Interessante.")
