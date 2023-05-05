ebb52d1369943fb5749335a2cf9a63ef1fb7d736
joaovictorgm <94790386+joaovictorgm@users.noreply.github.com>
2023-05-05 12:03:53 -0300

    Terminando o exercício de classes no DOM

--- treinando_classes_DOM/index.js
+++ treinando_classes_DOM/index.js
@@ -53,5 +53,50 @@ class FormComponent extends Component {
     // Método para adicionar elementos  como filhos 
     addChild(child)  {
         //Adiciona o elemento filho ao elemento do formulário 
+        this.#element.appendChild(child.build())
     }
 }
+
+// Classe especifica para elementos button, que deveser uma subclasse da classe Component
+class ButtonComponent extends Component {
+    constructor(text) {
+        // Chama o construtor da classe Component com a tagname 'button'
+        super('button');
+        // Define o conteúdo de texto do elemento com o parâmetro fornecido
+        this.#element.textContent = text;
+    }
+}
+// Classe específica para elementos div, que deve ser uma subclasse da classe Component
+class DivComponent extends Component {
+    constructor() {
+        //Chama o construtor da classe Component com a tagname 'div'
+        super('div');
+    }
+}
+
+// Classe específica para elementos span, que deve ser uma subclasse da classe Component
+class SpanComponent extends Component {
+    constructor() {
+        //Chama o construtor da class Component com a tagname 'span'
+        super('span');
+    }   
+}
+
+// Classe específica para elementos h1, que deve ser uma subclasse da classe Component
+class H1Component extends Component {
+    constructor(text) {
+        // Chama o construtor da classe Component com a tagname 'h1'
+        super('h1');
+        //Defina o conteúdo de texto do elemento com o parâmetro fornecido
+        this.#element.textContent = text;
+    }   
+}
+
+// Classe específica para elementos p, que deve ser uma subclasse da classe Component
+class PComponent extends Component {
+    constructor(text) {
+        //Chama o construtor da classe Component com a tagname 'p'
+        super('p')
+        //Define o conteúdo do elemento com o parâmetro fornecido
+    }
+} 
